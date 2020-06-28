0c223b662ad522b623ce5d082e11890edfd66ede
george-g <georgegombert@gmail.com>
2020-06-28 16:57:00 -0600

    api route paths and types all finished. No functionality yet

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,25 @@
 // var db = require("../models");
 
 module.exports = function(app) {
-  app.get("/api/images", function(req, res) {
+  app.get("/api/workouts", function(req, res) {
     db.Image.find({}).then(function(dbImages) {
       res.json(dbImages);
     });
   });
 
-  app.put("/api/images/:id", function(req, res) {
+  app.put("/api/workouts", function(req, res) {
+    db.Image.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function(dbImage) {
+      res.json(dbImage);
+    });
+  });
+
+  app.post("/api/workouts/:id", function(req, res) {
+    db.Image.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function(dbImage) {
+      res.json(dbImage);
+    });
+  });
+
+  app.get("/api/workouts/range", function(req, res) {
     db.Image.updateOne({ _id: req.params.id }, { rating: req.body.rating }).then(function(dbImage) {
       res.json(dbImage);
     });
